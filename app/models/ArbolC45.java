66780946686861cc82d8a993474189658583a5af
Claudia Bedoya <claudiabm777@gmail.com>
2015-11-30 05:12:07 -0500

    ddd

--- app/models/ArbolC45.java
+++ app/models/ArbolC45.java
@@ -76,7 +76,7 @@ public class ArbolC45 {
 	public String darPrediccion(){
 		try{
 
-			BufferedReader datafile = readDataFile("/public/trc.txt");
+			BufferedReader datafile = readDataFile("trc.txt");
 			BufferedReader datafile2 = readDataFile("/tmp/pred.txt");
 
 			Instances train = new Instances(datafile);
